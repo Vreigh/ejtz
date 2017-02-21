2e392597c2a115a6b4ee0202b9b7eabbf1cafd71
Vreigh <yahiko444@gmail.com>
2017-02-21 20:56:01 +0100

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,17 +3,25 @@
 @section('content')
 
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in!
+
+    <div class="row" style="text-align: center; margin-bottom: 20px;">
+        <h1>Wybierz przedmiot:</h1>
+    </div>
+
+
+    <div class="row subject-container">
+        @for($i=0; $i<10; $i++)
+            <div class="col col-sm-6 col-md-4 col-lg-3 subject-outer">
+                <div class="row">
+                    <div class="col-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 subject" style="background-color: grey;">
+                        <span>TO <span style="color: orange;">(1)</span> <span style="color: lightgreen";>(1)</span></span>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endfor
     </div>
+
 </div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    <link rel="stylesheet" href="{{asset("css/style1.css")}}">
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -47,7 +48,18 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            Przedmioty <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            @for($i=0; $i<10; $i++)
+                                <li><a href="{{ url('/logout') }}">TO</a></li>
+                            @endfor
+                        </ul>
+                    </li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
@@ -63,9 +75,15 @@
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/your-tasks') }}"><i class="fa fa-tasks" aria-hidden="true"></i> Twoje zadania</a></li>
+                                <li><a href="{{ url('/your-solutions') }}"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Twoje rozwiązania</a></li>
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
+
+                        <li><a data-toggle="tooltip" title="twoje punkty" data-placement="bottom"><i class="fa fa-star" aria-hidden="true"></i>100</a></li>
+                        <li><a data-toggle="tooltip" title="twoja waluta" data-placement="bottom"><i class="fa fa-star-half-o" aria-hidden="true"></i>200</a></li>
+
                     @endif
                 </ul>
             </div>
@@ -78,5 +96,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+    <script>
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
 </body>
 </html>
